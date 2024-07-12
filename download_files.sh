2fa971f479279263e5a870907c19457e56914feb
Harrison Wekesa Wanjala <51504073+Harrywekesa@users.noreply.github.com>
2024-07-12 16:38:14 +0300

    Update download_files.sh

--- download_files.sh
+++ download_files.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 # Download files from Google Drive
-echo "Downloading large files from Google Drive..."
+echo "Downloading files from Google Drive..."
 
-# Example for a single file
-curl -L -o <local-file-name> "<google-drive-shareable-link>"
+# List of files and their respective Google Drive links
+declare -A files
+files=(
+  ["rating.csv"]="https://drive.google.com/file/d/1oSuATe965VRX_yKRJhabwiylOTj2LTXS/view?usp=drive_link?id=rating.csv&export=download"
+  ["movie.csv"]="https://drive.google.com/file/d/1jcGysdgCbWoTLUgcsjtrVsoC3b8hb3w-/view?usp=drive_link?id=movie.csv&export=download"
+  #["file3.zip"]="https://drive.google.com/uc?id=FILE_ID_3&export=download"
+  # Add more files as needed
+)
 
-echo "Download completed."
+# Create a folder to store the downloaded files
+mkdir -p dataset
+
+# Download each file
+for file in "${!files[@]}"; do
+  echo "Downloading $file..."
+  curl -L -o "downloaded_files/$file" "${files[$file]}"
+done
+
+echo "All files downloaded."
